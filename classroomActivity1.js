a94ee7c554b049726395cb1e9aa1aaf2660a3b02
suganyatp <suganyatp@gmail.com>
2026-09-02 13:20:04 +0530

    Add files via upload
    Week1 of Day2 Classroom Activity that explains the datatypes

---
+++ classroomActivity1.js
@@ -0,0 +1,16 @@
+
+var firstName="Suganyaa"
+console.log(firstName);
+console.log(typeof firstName);
+var companyName="WIPRO TECHNOLOGIES"
+console.log(companyName);
+console.log(typeof companyName);
+var mobileNumber=9876543210
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+var isAutomation=true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
